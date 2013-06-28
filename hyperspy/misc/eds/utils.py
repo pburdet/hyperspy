4e53f35ffb74d8cac0f73fcf5992d96fb131ff00
pburdet <pb565@cam.ac.uk>
2013-06-28 11:38:07 +0100

    EHN: Xray range

--- hyperspy/misc/eds/utils.py
+++ hyperspy/misc/eds/utils.py
@@ -1,5 +1,44 @@
+import math
+import numpy as np
 
+from hyperspy.misc.eds.elements import elements as elements_db
     
 def _get_element_and_line(Xray_line):
     lim = Xray_line.find('_')
     return Xray_line[:lim], Xray_line[lim+1:]
+    
+    
+def xray_range(Xray_line,beam_energy,rho=None):
+    '''Return the Anderson-Hasler X-ray range.
+    
+    Parameters
+    ----------    
+    Xray_line: str
+        The X-ray line, e.g. Al_Ka
+        
+    beam_energy: float (kV)
+        The energy of the beam in kV. 
+        
+    rho: float (g/cm3)
+        The density of the material. If None, the density of the pure 
+        element is used.
+        
+    Returns
+    -------
+    X-ray range in micrometer.
+        
+    Notes
+    -----
+    From Anderson, C.A. and M.F. Hasler (1966). In proceedings of the 
+    4th international conference on X-ray optics and microanalysis.
+    
+    See also the textbook of Goldstein et al., Plenum publisher, 
+    third edition p 286
+    '''
+    element, line = _get_element_and_line(Xray_line)
+    if rho is None:
+        rho = elements_db[element]['density']
+    Xray_energy = elements_db[element]['Xray_energy'][line]
+    
+    return 0.064/rho*(np.power(beam_energy,1.68)-
+        np.power(Xray_energy,1.68))
