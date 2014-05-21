d6471693abd7777f0fde116166b69d88153ee93d
pburdet <pb565@cam.ac.uk>
2014-05-21 12:12:51 +0100

    TEST: new database

--- hyperspy/tests/signal/test_eds_sem.py
+++ hyperspy/tests/signal/test_eds_sem.py
@@ -509,7 +509,7 @@ class Test_tools_bulk:
             mp.Sample.xray_lines[0],
             mp.Acquisition_instrument.SEM.beam_energy,
             density=4.37499648818)
-        assert_equal(xr_range, 0.19002078834050035)
+        assert_equal(xr_range, 0.1900207, atol=1e-3)
 
 
 class Test_decomposition_model_from:
