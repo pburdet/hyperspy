20c1be8a23f6a3dcf4105b18075303607c644590
pburdet <pb565@cam.ac.uk>
2014-06-17 12:09:37 +0100

    NEW: get_mass_absorption_coefficient

--- hyperspy/misc/material.py
+++ hyperspy/misc/material.py
@@ -84,3 +84,66 @@ def density_of_mixture_of_pure_elements(elements, weight_percent):
         [elements_db[element]['Physical_properties']['density (g/cm^3)'] for element in elements])
     density = (weight_percent / densities / sum(weight_percent)).sum() ** -1
     return density
+    
+def _mac_interpolation(mac, mac1, energy,
+                       energy_db, energy_db1):
+    """
+    Interpolate between the tabulated mass absorption coefficients
+    for an energy
+
+    Parameters
+    ----------
+    mac, mac1: float
+        The mass absorption coefficients in cm^2/g
+    energy,energy_db,energy_db1:
+        The energy. The given energy and the tabulated energy,
+        respectively
+
+    Return
+    ------
+    mass absorption coefficient in cm^2/g
+    """
+    return np.exp(np.log(mac1) + np.log(mac / mac1)
+                  * (np.log(energy / energy_db1) / np.log(
+                      energy_db / energy_db1)))
+                                  
+def get_mass_absorption_coefficient(element, energies):
+    """
+    Get the mass absorption coefficient of a given energy (energies)
+
+    Parameters
+    ----------
+    element: str
+        The element symbol of the absorber, e.g. 'Al'.
+    energies: float or list of float
+        The energy or energies of the Xray in keV
+
+    Return
+    ------
+    mass absorption coefficient in cm^2/g
+    """
+    from hyperspy.misc.eds.ffast_mac import ffast_mac_db as ffast_mac
+    energies_db = ffast_mac[element].energies_keV
+    if hasattr(energies,'__iter__'):
+        is_iter = True
+    else:
+        is_iter = False
+        energies = [energies]
+    mac_res=[]
+    for energy in energies:
+        for index, energy_db in enumerate(energies_db):
+            if energy <= energy_db:
+                break
+        mac = ffast_mac[element].mass_absorption_coefficient_cm2g[index]
+        mac1 = ffast_mac[element].mass_absorption_coefficient_cm2g[index - 1]
+        energy_db = ffast_mac[element].energies_keV[index]
+        energy_db1 = ffast_mac[element].energies_keV[index - 1] 
+        if energy == energy_db or energy_db1 == 0:
+            mac_res.append(mac)
+        else:
+            mac_res.append(_mac_interpolation(mac, mac1, energy,
+                                      energy_db, energy_db1))
+    if is_iter:
+        return mac_res
+    else:
+        return mac_res[0]
