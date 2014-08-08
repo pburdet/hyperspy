BUG: units

hyperspy/_signals/eds.py
@@ -621,11 +621,14 @@ class EDSSpectrum(Spectrum):
         utils.misc.eds.model.continuous_xray_absorption
         edsmodel.add_background
         """
-
-        beam_energy = self._get_beam_energy()
+        
+        if self.axes_manager.signal_axes[0].units == 'eV' : 
+            units_factor = 1000.
+        else :
+            units_factor = 1.
+        beam_energy = self._get_beam_energy() / units_factor
         elements = self.metadata.Sample.elements
         TOA = self.get_take_off_angle()
-        units_name = self.axes_manager.signal_axes[0].units
         if weight_fraction == 'auto':
             if 'weight_fraction' in self.metadata.Sample:
                 weight_fraction = self.metadata.Sample.weight_fraction
@@ -639,15 +642,14 @@ class EDSSpectrum(Spectrum):
         energy_axis = spec.axes_manager.signal_axes[0]
         eng = np.linspace(energy_axis.low_value,
                           energy_axis.high_value,
-                          energy_axis.size)
+                          energy_axis.size) / units_factor
         eng = eng[np.searchsorted(eng, 0.0):]
         spec.data = np.append(np.array([0] * (len(spec.data) - len(eng))),
                               physical_model.continuous_xray_absorption(energy=eng,
                                                                         weight_fraction=weight_fraction,
                                                                         elements=elements,
                                                                         beam_energy=beam_energy,
-                                                                        TOA=TOA,
-                                                                        units_name=units_name))
+                                                                        TOA=TOA))
         return spec
 
     def get_sample_mass_absorption_coefficient(self,

hyperspy/misc/eds/physical_model.py
@@ -34,8 +34,7 @@ def continuous_xray_absorption(energy,
                                weight_fraction,
                                elements,
                                beam_energy,
-                               TOA,
-                               units_name):
+                               TOA):
     """Contninous X-ray Absorption within sample
 
     PDH equation (Philibert-Duncumb-Heinrich)
@@ -43,7 +42,7 @@ def continuous_xray_absorption(energy,
     Parameters
     ----------
     energy: float or list of float
-        The energy of the generated X-ray.
+        The energy of the generated X-ray in keV.
     weight_percent: list of float
         The sample composition
     elements: list of str
@@ -51,7 +50,7 @@ def continuous_xray_absorption(energy,
     TOA:
         the take off angle
     beam_energy:  float
-        The energy of the electron beam
+        The energy of the electron beam in keV.
     """
     h = 0
     for el, wt in zip(elements, weight_fraction):
@@ -59,10 +58,7 @@ def continuous_xray_absorption(energy,
         Z = utils.material.elements[el]['General_properties']['Z']
         h += wt * A / (Z * Z)
 
-    if units_name == 'eV':
-        coeff = 4.5 * 1e2
-    else:
-        coeff = 4.5 * 1e5
+    coeff = 4.5 * 1e5
 
     xi = np.array(utils.material.compound_mass_absorption_coefficient(
         energies=energy, elements=elements,